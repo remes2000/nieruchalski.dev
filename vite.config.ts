bb8d8b7a2f1582149005340751193da066432119
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2024-03-05 00:36:15 +0100

    feat: disable tracking with cookies

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig(({ mode }) => ({
             <script>
               var _paq = window._paq = window._paq || [];
               /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(["disableCookies"]);
               _paq.push(['trackPageView']);
               _paq.push(['enableLinkTracking']);
               (function() {
@@ -44,7 +45,7 @@ export default defineConfig(({ mode }) => ({
                 g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
               })();
             </script>
-            <!-- End Matomo Code -->          
+            <!-- End Matomo Code -->  
           `;
           route.contents = route.contents?.replace('</head>', `${matomoTag}</head>`);
         },
