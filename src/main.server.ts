fcfd09feb9e313ad6fc663382124f8aef356521a
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2025-11-27 19:54:40 +0100

    chore: update main.server

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,7 @@ import 'zone.js/node';
 import '@angular/platform-server/init';
 
 import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
 
 import { config } from './app/app.config.server';
@@ -12,8 +12,8 @@ if (import.meta.env.PROD) {
   enableProdMode();
 }
 
-export function bootstrap() {
-  return bootstrapApplication(AppComponent, config);
+export function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, config, context);
 }
 
 export default async function render(url: string, document: string) {
